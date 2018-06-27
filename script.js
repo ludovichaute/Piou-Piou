4a49bc9bb1b8f2b08d7893390e898e3f860b8135
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-27 16:56:33 +0200

    piou-piou init

--- script.js
+++ script.js
@@ -6,30 +6,31 @@ window.addEventListener("keydown", checkKeyPressed, false);
 let width = 25;
 let heigth = 25;
 
-
-
 function checkKeyPressed(e) {
-    
     if (e.keyCode == "39") {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        let x = 0;
-        x++;
-        console.log("droite "+x);
+        let right = 0;
+        right += 2;
+        console.log("droite "+right);
         ctx.fillStyle = '#0AA';
-        ctx.translate(x, 0);
-        var img = document.getElementById("canon"); 
-        ctx.drawImage(img, 10, 10, width, heigth);         
-    } if (e.keyCode == "37") {
+        ctx.translate(right, 0);
+        var img = document.getElementById("canon_right"); 
+        ctx.drawImage(img, 15, 15, width, heigth); 
+    } 
+    if (e.keyCode == "37") {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        let e = 0;
-        e--;
-        console.log("gauche "+e);
+        let left = 0;
+        left-= 2;
+        console.log("gauche "+left);
         ctx.fillStyle = '#0AA';
-        ctx.translate(e, 0); 
-        var img = document.getElementById("canon"); 
-        ctx.drawImage(img, 10, 10, width, heigth); 
+        ctx.translate(left, 0); 
+        var img = document.getElementById("canon_left"); 
+        ctx.drawImage(img, 15, 15, width, heigth); 
     }
-    
+    if (e.keyCode == "32") {
+        console.log("tir");
+        
+    }     
 }   
 
 
